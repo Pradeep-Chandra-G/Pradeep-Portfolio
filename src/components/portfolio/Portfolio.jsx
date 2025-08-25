f622e0d6320eaf946c62ce9c555e092403af63ac
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-08-25 16:22:25 +0530

    Fixed see demo button

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,25 +8,27 @@ const items = [
     title: "SchooLama LMS",
     img: "/schoolama.png",
     desc: "SchooLama AI LMS empowers students and educators with intelligent tools for seamless learning. Adaptive courses, instant feedback, and smart analytics ensure growth, while AI-driven insights simplify teaching. Engaging, scalable, and efficient, it transforms education into a guided, personalized journey.",
+    demo: "https://schoolama.studio/"
   },
   {
     id: 2,
     title: "SchooLama Blog",
     img: "/blog.png",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "My Schoolama blog is built with React, Vite, and Tailwind CSS to deliver speed, style, and ease. It supports markdown, images, videos, and comes with modern security. Clean UI, responsive design, and flexible tools make sharing and learning seamless.",
+    demo: "https://schoolama-blog-frontend-030bedd2be82.herokuapp.com/"
   },
-  {
-    id: 3,
-    title: "Vanilla JS App",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-  },
-  {
-    id: 4,
-    title: "Music App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-  },
+//   {
+//     id: 3,
+//     title: "Vanilla JS App",
+//     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+//   },
+//   {
+//     id: 4,
+//     title: "Music App",
+//     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+//   },
 ];
 
 const Single = ({ item }) => {
@@ -48,7 +50,7 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <button onClick={() => window.open(item.demo, "_blank")}>See Demo</button>
           </motion.div>
         </div>
       </div>
