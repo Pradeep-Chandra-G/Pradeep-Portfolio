248d849ab7b43f80aed005f5d3e94a75b6ec1ce4
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-09-17 17:45:11 +0530

    Removed dribble link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,9 +29,9 @@ const Navbar = () => {
           <a href="/Pradeep's Resume.pdf" target="_blank">
             <img src="/resume.png" alt="Pradeep's Resume" />
           </a>
-          <a href="#" target="_blank">
+          {/* <a href="#" target="_blank">
             <img src="/dribbble.png" alt="Dribble Link" />
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
