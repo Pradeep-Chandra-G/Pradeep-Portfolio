5bc8489511b284537d9af06ef79b206c683134af
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-09-17 19:19:30 +0530

    Added tool tips to links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
+import { Tooltip } from "react-tooltip";
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      <Tooltip id="my-tooltip" />
       {/* Sidebar */}
       <Sidebar />
       <div className="wrapper">
@@ -17,19 +19,36 @@ const Navbar = () => {
           together!
         </motion.span>
         <div className="social">
-          <a href="https://github.com/Pradeep-Chandra-G" target="_blank">
+          <a
+            href="https://github.com/Pradeep-Chandra-G"
+            target="_blank"
+            data-tooltip-id="my-tooltip"
+            data-tooltip-content="Go to my Github!"
+          >
             <img src="/github-logo.png" alt="Github Link" />
           </a>
           <a
             href="https://www.linkedin.com/in/pradeep-chandra-g/"
             target="_blank"
+            data-tooltip-id="my-tooltip"
+            data-tooltip-content="Go to my LinkedIn!"
           >
             <img src="/linkedin.png" alt="LinkedIN Link" />
           </a>
-          <a href="/Pradeep's Resume.pdf" target="_blank">
+          <a
+            href="/Pradeep's Resume.pdf"
+            target="_blank"
+            data-tooltip-id="my-tooltip"
+            data-tooltip-content="Go to my Resume!"
+          >
             <img src="/resume.png" alt="Pradeep's Resume" />
           </a>
-          <a href="https://dribbble.com/pradeep-chandra" target="_blank">
+          <a
+            href="https://dribbble.com/pradeep-chandra"
+            target="_blank"
+            data-tooltip-id="my-tooltip"
+            data-tooltip-content="Go to my Dribbble!"
+          >
             <img src="/dribbble.png" alt="Dribble Link" />
           </a>
         </div>
